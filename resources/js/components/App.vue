c87498f4c442325751f9e9826e6d1cba058cccc8
anisimovadim <15362637aA>
2024-08-07 11:32:09 +0300

    add api

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -4,7 +4,7 @@
             <main-page></main-page>
             <panel-invent></panel-invent>
         </div>
-    <modal-block></modal-block>
+    <modal-block :inventory="inventory" @submit-form="submitForm"></modal-block>
 </template>
 
 <script>
@@ -12,9 +12,33 @@ import MainPage from "./pages/MainPage.vue";
 import MainPanel from "./layouts/MainPanel.vue";
 import PanelInvent from "./layouts/PanelInvent.vue";
 import ModalBlock from "./ui/ModalBlock.vue";
+import axios from "axios";
 export default {
     name: "App",
-    components: {MainPage, MainPanel, PanelInvent, ModalBlock}
+    components: {MainPage, MainPanel, PanelInvent, ModalBlock},
+    data(){
+        return{
+            inventory:{
+                type:'',
+                model:'',
+                character:'',
+                status:'no_select',
+                cabinet:''
+            }
+        }
+    },
+    methods:{
+        submitForm(){
+            console.log(this.inventory);
+            axios.post('/api/create/inventory', this.inventory)
+                .then(response=>{
+                    console.log(response)
+                })
+                .catch(error=>{
+                    console.log(error)
+                })
+        }
+    }
 
 }
 </script>
--- resources/js/components/ui/ModalBlock.vue
+++ resources/js/components/ui/ModalBlock.vue
@@ -6,22 +6,22 @@
                     <h1 class="modal-title fs-5" id="createInventoryModalLabel">Добавить инвентарь</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form action="">
+                <form @submit.prevent="$emit('submitForm')">
                     <div class="modal-body">
                         <div class="mb-3">
                             <label for="type" class="form-label">Введите тип</label>
-                            <input type="text" placeholder="Например: ноутбук" id="type" class="form-control" required>
+                            <input type="text" placeholder="Например: ноутбук" v-model="inventory.type" id="type" class="form-control" required>
                         </div>
                         <div class="mb-3">
                             <label for="model" class="form-label">Введите модель</label>
-                            <input type="text" placeholder="Например: Acer m342t" id="model" class="form-control" required>
+                            <input type="text" placeholder="Например: Acer m342t" v-model="inventory.model" id="model" class="form-control" required>
                         </div>
                         <div class="mb-3">
                             <label for="character" class="form-label">Описание инвентаря</label>
-                            <textarea name="character" id="character" cols="30" rows="5" class="form-control"></textarea>
+                            <textarea name="character" id="character" v-model="inventory.character" cols="30" rows="5" class="form-control"></textarea>
                         </div>
                         <div class="mb-3">
-                            <select name="status" id="status" class="form-select">
+                            <select name="status" id="status" class="form-select" v-model="inventory.status">
                                 <option value="no_select" selected disabled>Укажите состояние инвентаря</option>
                                 <option value="good">Рабочее</option>
                                 <option value="bad">Не рабочее</option>
@@ -29,7 +29,7 @@
                         </div>
                         <div class="mb-3">
                             <label for="cabinet" class="form-label">Укажите кабинет для выдачи</label>
-                            <input type="text" placeholder="Например: 220 или оставьте пустым" id="cabinet" class="form-control">
+                            <input type="text" placeholder="Например: 220 или оставьте пустым" v-model="inventory.cabinet" id="cabinet" class="form-control">
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -43,7 +43,10 @@
 
 <script>
 export default {
-    name: "ModalBlock"
+    name: "ModalBlock",
+    props:{
+        inventory: Object
+    }
 }
 </script>
 
