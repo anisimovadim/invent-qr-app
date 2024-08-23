9a010ffb2e046b7f372b8b18d49e799705ad5169
anisimovadim <15362637aA>
2024-08-23 15:42:56 +0300

    add edite info panel

--- resources/js/components/layouts/PanelInvent.vue
+++ resources/js/components/layouts/PanelInvent.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="panel shadow">
-    <div class="qrcode">
-        <img :src="inventory['src_qrcode']" alt="">
-    </div>
+
+        <div class="qrcode" v-if="inventory['src_qrcode']">
+            <img :src="inventory['src_qrcode']" alt="">
+        </div>
+        <div class="info p-5">
+            <p v-if="inventory.whome"><b>Кому выдан:</b> {{inventory.whome}}</p>
+            <p v-if="inventory.cabinet"><b>Здание №{{inventory.number_building}} Кабинет: </b>{{inventory.cabinet}}</p>
+            <p v-if="inventory.character"><b>Характеристика: </b>{{inventory.character}}</p>
+        </div>
+
 </div>
 </template>
 
@@ -18,5 +25,12 @@ export default {
 <style scoped>
 .panel{
     width: 500px;
+}
+.qrcode{
+    max-width: 250px;
+    margin: auto;
+}
+img{
+    width: 100%;
 }
 </style>
